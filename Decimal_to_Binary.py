fe60a371f69a2a044d77374ca0936141c689d4fb
JyotirmoyeeM <89502515+JyotirmoyeeM@users.noreply.github.com>
2025-02-28 22:51:57 +0530

    Update Decimal_to_Binary.py

--- Decimal_to_Binary.py
+++ Decimal_to_Binary.py
@@ -1,5 +1,5 @@
 #WAP to convert decimal to binary
-N=2
+N=int(input())
 ans = " "
 if N==0:
     print(0)
@@ -11,3 +11,6 @@ else:
             ans = "1" + ans
         N//=2
     print(ans)
+    """
+    The logic used here is that whenever we try to convert an integer (+ve) integer to decimal, we divide it by 2 until we can reduce the integer to 1 or 0. Then we cumulate the remainder every time we divide and find our result. 
+    """
