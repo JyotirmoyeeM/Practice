1bab388619ee79104041bdce12b83b2d3bf1f409
Jyotirmoyee Mitra <89502515+JyotirmoyeeM@users.noreply.github.com>
2025-03-19 15:04:13 +0530

    Update Square_patterns.py

--- Square_patterns.py
+++ Square_patterns.py
@@ -1,33 +1,66 @@
-“””to print pattern 
-(1)    * * * *
-		   * * * *
-		   * * * *
-		   * * * *
+“”” to print pattern 
+(1)     * * * *
+	* * * *
+	* * * *
+	* * * *
 
  (2)    1234
         1234
         1234
         1234
 
-(3)     1111
+ (3)    4321
+	4321
+	4321
+	4321
+
+ (4)    1111
         2222
         3333
         4444
+
+ (5)	4444
+	3333
+	2222
+	1111
   
 “””
 N = 4
 
 def star_square_pattern(N):
-    for i in range(1, N + 1):
-        for j in range(1, N + 1):
+    for i in range(1, N + 1, 1):
+        for j in range(1, N + 1,1):
             print("*", end="")
         print()
+
 def accending_integer_square_pattern(N):
     for i in range(1,N + 1, 1):
         for j in range(1, N + 1, 1):
             print(j, end="")
         print()
+def decending_integer_square_pattern(N):
+    for i in range(1,N+1,1):
+        for j in range(N, 0, -1):
+            print(j, end ="")
+        print()
+def increasing_integer_square_pattern(N):
+    for i in range(1,N + 1, 1):
+        for j in range(1, N + 1, 1):
+            print(i, end="")
+        print()
 
+def decreasing_integer_square_pattern(N):
+    for i in range(1,N + 1, 1):
+        for j in range(N, 0, -1):
+            print(j, end="")
+        print()
+        
 star_square_pattern(N)
 print()
 accending_integer_square_pattern(N)
+print()
+decending_integer_square_pattern(N)
+print()
+increasing_integer_square_pattern(N)
+print()
+decreasing_integer_square_pattern(N)
