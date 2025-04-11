0a725fa5114bb4dd047fcc9207fa823e17de4265
Jyotirmoyee Mitra <89502515+JyotirmoyeeM@users.noreply.github.com>
2025-04-11 15:12:44 +0530

    Create Merge_two_dict.py

---
+++ Dictionar/Merge_two_dict.py
@@ -0,0 +1,10 @@
+#Write a program to merge two dictionaries.
+# create two dictionaries
+A = {12: 'Kathmandu', 11: 'London', 3: 'Sydney'}
+B = {10: 'New York', 2: 'Delhi'}
+
+# merge the two dictionaries using update()
+A.update(B)
+
+# print the merged dictionary
+print(A)
