d152efdabb2ec0cb168de23fdfb71a001f934aff
Jyotirmoyee Mitra <jyotirmoyee25mitra@gmail.com>
2025-09-05 19:31:43 +0530

    files moved

--- Python-Basic/while-loop.py
+++ Python-Basic/while-loop.py
@@ -1,6 +1,6 @@
 #Write a program to print a word multiple times using a while loop.
-n = int(input())
-word = input()
+n = int(input("Enter number: "))
+word = input("Enter word: ")
 count = 1
 while count <= n:
   print(word)
